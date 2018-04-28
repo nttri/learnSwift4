c93b6e29beea5c62b0c6d836a71f43d88a7d98f6
nttri <bem0302@gmail.com>
2018-04-29 01:10:43 +0700

    update UI
    log in screen

--- demo/demo2704-firebase/demo2704-firebase/ViewController.swift
+++ demo/demo2704-firebase/demo2704-firebase/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var screenHeight:CGFloat = 0
+    
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.updateUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    private func updateUI(){
+        self.screenHeight = UIScreen.main.bounds.height
+        
+        appTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: self.screenHeight*0.22).isActive = true
+        emailLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: self.screenHeight*0.55).isActive = true
+    }
 
 }
 
