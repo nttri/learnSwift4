7dc6046f68d80a4414494de2f676a0047774dfbf
nttri <bem0302@gmail.com>
2018-05-02 01:35:02 +0700

    Add authen()
    add register & login method

--- demo/demo2704-firebase/demo2704-firebase/ViewController.swift
+++ demo/demo2704-firebase/demo2704-firebase/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -14,11 +15,51 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var appTitle: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func registerTouched(_ sender: Any) {
+        
+        let email:String? = emailField.text
+        let password:String? = passwordField.text
+        
+        Auth.auth().createUser(withEmail: email!, password: password!, completion: {(user,error) in
+            if error != nil {
+                print("register failed")
+            }else{
+                print("register completed")
+            }
+        })
+    }
+    
+    @IBAction func loginTouched(_ sender: Any) {
+        
+        let email:String? = emailField.text
+        
+        let actionCodeSettings = ActionCodeSettings()
+        actionCodeSettings.url = URL(string: "https://www.example.com")
+        // The sign-in operation has to always be completed in the app.
+        actionCodeSettings.handleCodeInApp = true
+        actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
+        actionCodeSettings.setAndroidPackageName("com.example.android", installIfNotAvailable: false, minimumVersion: "12")
+        
+        Auth.auth().sendSignInLink(toEmail: email!, actionCodeSettings: actionCodeSettings)
+        { error in
+            if error != nil {
+                print("Log in success")
+            }else{
+                print("Log in failed")
+            }
+            UserDefaults.standard.set(email, forKey: "Email")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.updateUI()
+        
     }
 
     override func didReceiveMemoryWarning() {
